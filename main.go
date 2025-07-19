cdaa7e1b8aa2de7fa9ac8459321ba98db7d36e4a
DhaniKWP <dhanikusuma256@gmail.com>
2025-07-19 16:24:05 +0700

    membuat endpoint

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main 
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID					string			`json:"id"`
+	Item 				string	 		`json:"item"`
+	Completed 	bool 				`json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Belajar Golang", Completed: false},
+	{ID: "2", Item: "Belajar RestAPI", Completed: false},
+	{ID: "3", Item: "Belajar Gin", Completed: false},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func addTodos(context *gin.Context){
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated,newTodo)
+}
+
+func main() {
+	Router := gin.Default()
+	Router.GET("/todos", getTodos)
+	Router.POST("/todos", addTodos)
+	Router.Run("localhost:9090")
+}
